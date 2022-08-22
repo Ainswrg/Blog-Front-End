47387af679a8ddfa9a44425de32e0832b4c361db
Jaslan <Jaslan.dlnv@gmail.com>
2022-08-22 11:00:09 +0500

    feat: add Home page and Post

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import Container from "@mui/material/Container";
 import React from "react";
 import { Header } from "./components";
+import { Home } from "./pages/Home";
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
+      <Container maxWidth="lg">
+        <Home />
+      </Container>
     </>
   );
 };
---
+++ src/components/UserInfo/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./UserInfo.module.scss";
+
+type UserInfoProps = {
+  avatarUrl?: string;
+  fullName?: string;
+  additionalText: string;
+};
+
+export const UserInfo: React.FC<UserInfoProps> = ({
+  avatarUrl,
+  fullName,
+  additionalText,
+}) => {
+  return (
+    <div className={styles.root}>
+      <img
+        className={styles.avatar}
+        src={avatarUrl || "/noavatar.png"}
+        alt={fullName}
+      />
+      <div className={styles.userDetails}>
+        <span className={styles.userName}>{fullName}</span>
+        <span className={styles.additional}>{additionalText}</span>
+      </div>
+    </div>
+  );
+};
